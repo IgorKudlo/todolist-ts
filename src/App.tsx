b075d3771929835e37425921ae490b2b40926673
Igor Kudlo <igor.kudlo@spiralscout.com>
2022-03-16 00:14:47 +0300

    TS-01 add components Todolist, work with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {TaskType, Todolist} from "./Todolist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let task1: Array<TaskType> = [
+        {id: 1, title: "CSS&HTML", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false}
+    ];
+
+    let task2: Array<TaskType> = [
+        {id: 1, title: "Terminator", isDone: true},
+        {id: 2, title: "XXX", isDone: false},
+        {id: 3, title: "Jentlments of fortune", isDone: true}
+    ];
+
+    return (
+        <div className="App">
+            <Todolist title={"What to learn"} tasks={task1}/>
+            <Todolist title={"Movies"} tasks={task2}/>
+        </div>
+    );
 }
 
 export default App;
