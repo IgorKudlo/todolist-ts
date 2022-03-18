6c90af4b3db6398cce74d357abfed307c471c515
Igor Kudlo <igor.kudlo@spiralscout.com>
2022-03-18 12:04:11 +0300

    Tl-03 add listener handler(addTask, removeTask, filtered); add uuid for generate id

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import React, {useState} from "react";
 import {TaskType, Todolist} from "./Todolist";
+import { v1 } from "uuid";
 import "./App.css";
 
 export type FilterValuesType = "all" | "completed" | "active";
 
 function App() {
     let [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: 1, title: "CSS&HTML", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "React", isDone: false}
+        {id: v1(), title: "CSS&HTML", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
+        {id: v1(), title: "Rest API", isDone: false},
+        {id: v1(), title: "GraphQL", isDone: false},
     ]);
     let [filter, setFilter] = useState<FilterValuesType>("all");
 
-    function removeTask(id: number) {
+    function removeTask(id: string) {
         let filteredTasks = tasks.filter(t => t.id !== id);
         setTasks(filteredTasks);
+    }
+
+    function addTask(title: string) {
+        let newTask = { id: v1(), title, isDone: false };
+        let newTasks = [newTask, ...tasks];
+        setTasks(newTasks);
     }
 
     function changeFilter(value: FilterValuesType) {
@@ -32,6 +41,7 @@ function App() {
                 tasks={tasksForTodolist}
                 removeTask={removeTask}
                 changeFilter={changeFilter}
+                addTask={addTask}
             />
         </div>
     );
